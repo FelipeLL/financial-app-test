1aea4d811eefc5b31c5342805a516a7df18fbef6
FelipeLL <f3lip3.3z@hotmail.com>
2024-10-28 15:34:22 -0500

    refactor(home): move home component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,39 @@
-import MainContent from '@/components/home';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Home() {
-  return <MainContent />;
+  const cards = [
+    {
+      title: 'Sistema de gestión de ingresos y gastos',
+      content: 'Accede y administra tus ingresos y gastos de manera eficiente.',
+    },
+    {
+      title: 'Gestión de usuarios',
+      content:
+        'Administra los usuarios de la plataforma, controlando el acceso a las funcionalidades.',
+    },
+    {
+      title: 'Reportes',
+      content:
+        'Visualiza y descarga reportes financieros detallados sobre ingresos y gastos.',
+    },
+  ];
+  return (
+    <div className='flex justify-around items-center p-8 h-full'>
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          className='w-1/4 h-52 shadow-lg transition-transform duration-200 hover:scale-105 hover:shadow-xl rounded-lg cursor-pointer'
+        >
+          <CardHeader>
+            <CardTitle className='text-center text-lg font-semibold text-gray-800'>
+              {card.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className='text-center text-gray-600'>
+            {card.content}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
