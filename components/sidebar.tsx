a2a409164eee00771debfa88b2c6a6a09ab0651c
FelipeLL <f3lip3.3z@hotmail.com>
2024-10-28 18:55:59 -0500

    feat(sidebar): add link highlight and logout button

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,23 +1,33 @@
-import Link from 'next/link';
 import { FC } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Home, DollarSign, Users, BarChart, LogOut } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
 
 const Sidebar: FC = () => {
+  const router = useRouter();
+
   const routes = [
     {
       label: 'Inicio',
       href: '/',
+      icon: Home,
     },
     {
       label: 'Ingresos y egresos',
       href: '/transactions',
+      icon: DollarSign,
     },
     {
       label: 'Usuarios',
       href: '/users',
+      icon: Users,
     },
     {
       label: 'Reportes',
       href: '/reports',
+      icon: BarChart,
     },
   ];
 
@@ -28,16 +38,34 @@ const Sidebar: FC = () => {
       </div>
 
       <nav className='w-full'>
-        {routes.map((route, index) => (
-          <Link
-            key={index}
-            href={route.href}
-            className='block w-full text-center p-3 mb-4 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors duration-200 hover:shadow-md'
-          >
-            {route.label}
-          </Link>
-        ))}
+        {routes.map((route, index) => {
+          const Icon = route.icon;
+          return (
+            <Link
+              key={index}
+              href={route.href}
+              className={cn(
+                'block w-full p-3 mb-4 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors duration-200 hover:shadow-md',
+                {
+                  'bg-gray-500': router.pathname === route.href,
+                }
+              )}
+            >
+              <div className='flex gap-4'>
+                <Icon />
+                <p>{route.label}</p>
+              </div>
+            </Link>
+          );
+        })}
       </nav>
+
+      <div className='block w-full mt-auto  p-3 mb-4 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors duration-200 hover:shadow-md cursor-pointer'>
+        <div className='flex justify-center gap-2'>
+          <LogOut />
+          <p>Cerrar sesión</p>
+        </div>
+      </div>
     </aside>
   );
 };
