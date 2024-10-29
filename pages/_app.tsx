6e86e6ba6b75a7bcd9a1787bedcbf1fa62c848bf
FelipeLL <f3lip3.3z@hotmail.com>
2024-10-28 19:33:57 -0500

    feat(pages): setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: '/api/graphql',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
